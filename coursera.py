e6a8bfc408e82e0abe740fe4157c107a611b15f8
xDSUP <ilya.elfimow@yandex.ru>
2017-01-31 18:44:32 +0300

    ver 1.0.3

--- coursera.py
+++ coursera.py
@@ -1,5 +1,7 @@
 import requests
 import re
+import argparse
+import sys
 
 from random import sample
 from io import BytesIO
@@ -8,47 +10,54 @@ from bs4 import BeautifulSoup
 from openpyxl import Workbook
 
 
-def get_courses_list(html, courses_count=20):
+def get_courses_list_html():
+    website_address = "https://www.coursera.org/sitemap~www~courses.xml"
+    return requests.get(website_address).content
+    
+
+def get_random_courses(html, courses_count=20):
     tree = etree.parse(BytesIO(html))
     root = tree.getroot()
     courses_urls = sample([url[0].text for url in root], courses_count)
     
     courses_list = []
     for url in courses_urls:
-        course = get_course_info(url)
+        course = get_course_info(get_course_html(url),url)
         if course is not None:
             courses_list.append(course)
     return courses_list
 
 
-def get_course_info(course_slug):
-    course_html = requests.get(course_slug).text
-    soup = BeautifulSoup(course_html, "lxml")
-    number_weeks = 0
-    for week in soup.findAll(class_="week"):
-        number_weeks += 1
-    
+def get_course_html(course_slug):
+    course_html = requests.get(course_slug)
+    return course_html.text
+
+
+def get_course_info(raw_html, corse_url):
+    soup = BeautifulSoup(raw_html, "lxml")
+    number_weeks = len(soup.findAll(class_="week"))
     course_info = {}
     course_info['title'] = soup.find(class_='title display-3-text').text
     course_info['language'] = soup.find(class_='language-info').text
-    course_info['week'] = number_weeks
-    course_info['course_url'] = course_slug
+    course_info['week'] = number_weeks if number_weeks != 0 else "No info"
+    course_info['course_url'] = corse_url
     course_info['starts'] = get_starts(soup)
     course_info['rating'] = get_rating(soup)
     return course_info
 
 
 def get_starts(soup):
-    start_date = soup.find(class_='startdate rc-StartDateString caption-text').text
-    return start_date
+    start_date = soup.find('div', {'class':'startdate'})
+    if start_date:
+        return start_date.text
+    return "No info"
     
 
 def get_rating(soup):
-    rating = None
     result_tag = soup.find(class_='ratings-text bt3-hidden-xs')
-    if result_tag:
-        rating = re.search(r"\d+.\d+", result_tag.text).group(0)
-    return rating   
+    if result_tag is not None:
+        return re.search(r"\d+.\d+", result_tag.text).group(0)
+    return "No info"
     
 
 def output_courses_info_to_xlsx(filepath, courses):
@@ -67,10 +76,17 @@ def output_courses_info_to_xlsx(filepath, courses):
 
 
 if __name__ == '__main__':
-    website_address = "https://www.coursera.org/sitemap~www~courses.xml"
-    html = requests.get(website_address).content
-    courses_list = get_courses_list(html)
-    filepath = input("Enter filepath (.xlsx): ")
+    parser = argparse.ArgumentParser(
+        description=('The script obtains Coursera courses information and unloads it to the xlsx-file'))
+    parser.add_argument('xlsx_file_name', help='xlsx-file name')
+    if len(sys.argv) < 2:
+        filepath = "Result.xlsx"
+    else:
+        args = parser.parse_args()
+        filepath = args.xlsx_file_name
+    
+    html = get_courses_list_html()
+    courses_list = get_random_courses(html)
     output_courses_info_to_xlsx(filepath, courses_list)
     print("Finish")
-    
+
