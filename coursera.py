311e377b8876eaa649b5006a75483e5b37546c8f
xDSUP <ilya.elfimow@yandex.ru>
2017-02-01 21:54:27 +0300

    Использовал argparse, немного поменял функции

--- coursera.py
+++ coursera.py
@@ -26,6 +26,33 @@ def get_random_courses(html, courses_count=20):
         if course is not None:
             courses_list.append(course)
     return courses_list
+import requests
+import re
+import argparse
+
+from random import sample
+from io import BytesIO
+from lxml import etree
+from bs4 import BeautifulSoup
+from openpyxl import Workbook
+
+
+def get_courses_list_html():
+    website_address = "https://www.coursera.org/sitemap~www~courses.xml"
+    return requests.get(website_address).content
+    
+
+def get_random_courses(html, courses_count):
+    tree = etree.parse(BytesIO(html))
+    root = tree.getroot()
+    courses_urls = sample([url[0].text for url in root], courses_count)
+    
+    courses_list = []
+    for url in courses_urls:
+        course = get_course_info(get_course_html(url),url)
+        if course is not None:
+            courses_list.append(course)
+    return courses_list
 
 
 def get_course_html(course_slug):
@@ -60,15 +87,14 @@ def get_rating(soup):
     return "No info"
     
 
-def output_courses_info_to_xlsx(filepath, courses):
+def output_courses_info_to_xlsx(filepath, courses, keys):
     wb = Workbook()
     ws = wb.active
-    keys = ['title', 'starts', 'language', 'week', 'rating', 'course_url']
-
+    
     for column, key in enumerate(keys, start=1):
         ws.cell(row=1, column=column, value=key)
 
-    for row, item in enumerate(courses, start=2):
+    for row, item in enumerate(courses, start=1):
         for column, key in enumerate(keys, start=1):
             ws.cell(row=row, column=column, value=item[key])
 
@@ -77,16 +103,15 @@ def output_courses_info_to_xlsx(filepath, courses):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description=('The script obtains Coursera courses information and unloads it to the xlsx-file'))
-    parser.add_argument('xlsx_file_name', help='xlsx-file name')
-    if len(sys.argv) < 2:
-        filepath = "Result.xlsx"
-    else:
-        args = parser.parse_args()
-        filepath = args.xlsx_file_name
+        description='The script obtains Coursera courses information and unloads it to the xlsx-file')
+    parser.add_argument('-fn', '--xlsx_file_name', default="Result.xlsx", 
+                        help='xlsx-file name')
+    parser.add_argument('-c', '--count', default=20, 
+                        help='Count courses', type=int)
+    args = parser.parse_args()
     
     html = get_courses_list_html()
-    courses_list = get_random_courses(html)
-    output_courses_info_to_xlsx(filepath, courses_list)
+    courses_list = get_random_courses(html, args.count)
+    keys = ['title', 'starts', 'language', 'week', 'rating', 'course_url']
+    output_courses_info_to_xlsx(args.xlsx_file_name, courses_list, keys)
     print("Finish")
-
